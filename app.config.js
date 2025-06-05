37061baa1d17caca1621740bf615648bfd891f33
Yen <s810643@gmail.com>
2025-06-06 00:22:39 +0800

    add project id

--- app.config.js
+++ app.config.js
@@ -1,9 +1,15 @@
 export default {
   expo: {
+    name: 'd-leap-app',
+    slug: 'd-leap-app',
     // ...其他設定
     android: {
-      // ...其他設定
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      googleServicesFile: './android/app/google-services.json',
+    },
+    extra: {
+      eas: {
+        projectId: '4a4bf6c2-bfba-4071-9f6f-3014b76069af',
+      },
     },
   },
 };
